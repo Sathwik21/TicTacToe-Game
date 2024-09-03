0ff64d30bd1f4b930c6b27681f509ec46a55a96f
Sathwik <snalla1@uco.edu>
2024-09-03 16:56:26 -0500

    initial commit

---
+++ public/controller/firebase_core.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBbitNrE3Js_xvBgsWH9yrb5De82Ki1_dM",
+    authDomain: "snalla1-cmsc5373.firebaseapp.com",
+    projectId: "snalla1-cmsc5373",
+    storageBucket: "snalla1-cmsc5373.appspot.com",
+    messagingSenderId: "93333849445",
+    appId: "1:93333849445:web:368d6c1f88fc7f5f7d4d8c"
+  };
+
+  // Initialize Firebase
+  export const app = initializeApp(firebaseConfig);
