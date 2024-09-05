11207ef48571be9b3d6f7d9d7cdfcb1c19098cac
Sathwik <snalla1@uco.edu>
2024-09-05 00:43:50 -0500

    final product ready

--- public/app.js
+++ public/app.js
@@ -1,1 +1,24 @@
-console.log('from app.js')
+import { onClickHomeMenu, onClickMenu2Menu, onClickSignoutMenu } from "./controller/menueventhandlers.js";
+import { attachAuthStateChangedObserver } from "./controller/firebase_auth.js";
+import { routing } from "./controller/route_controller.js";
+
+//menu button handlers
+document.getElementById('menu-home').onclick = onClickHomeMenu;
+document.getElementById('menu-menu2').onclick = onClickMenu2Menu;
+document.getElementById('menu-signout').onclick = onClickSignoutMenu;
+
+attachAuthStateChangedObserver();
+
+window.onload = function(e) {
+    const pathname = window.location.pathname;
+    const hash = window.location.hash;
+    console.log(pathname,hash);
+    routing(pathname, hash);
+}
+
+window.onpopstate = function(e) {
+    e.preventDefault();
+    const pathname = window.location.pathname;
+    const hash = window.location.hash;
+    routing(pathname, hash);
+}
---
+++ public/view/home_page.js
@@ -0,0 +1,22 @@
+import { root } from "./elements.js";
+import { currentUser } from "../controller/firebase_auth.js";
+import { protectedView } from "./protected_view.js";
+import { onSubmitCalcForm } from "../controller/home_controller.js";
+
+export async function homePageView() {
+     if(!currentUser) {
+          root.innerHTML = await protectedView();
+          return;
+     }
+     const response = await fetch('/view/templates/home_page_template.html',
+          {cache: 'no-store'});
+     const divWrapper = document.createElement('div');
+     divWrapper.innerHTML = await response.text();
+     divWrapper.classList.add('m-4', 'p-4');
+
+     const form  = divWrapper.querySelector('form');
+     form.onsubmit = onSubmitCalcForm;
+     
+     root.innerHTML = '';
+     root.appendChild(divWrapper);
+}
